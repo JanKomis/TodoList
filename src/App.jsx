2691625e8fb91df939c1915d8d379dcfc16e6c26
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2023-09-18 13:26:37 +0200

    hlavní struktura apky

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ControlContainer />
+      <ItemContainer />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>jbjbbjb</h1>;
+}
+
+function Footer() {
+  return <h1>jbjbbjb</h1>;
+}
+
+function ControlContainer() {
+  return <h1>jbjbbjb</h1>;
+}
+
+function ItemContainer() {
+  return <h1>jbjbbjb</h1>;
+}
+
+function Button() {
+  return <h1>jbjbbjb</h1>;
+}
+
+export default App;
