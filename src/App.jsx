63eb7e8087d47095c39c4f21d5cd9bb3b0429442
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2023-09-18 15:07:04 +0200

    doplněná základní struktura apky

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 
+
+
 function App() {
   return (
     <>
       <Header />
-      <ControlContainer />
+      <ControlContainer>
+        <Button>Add</Button>
+        <InputLabel>Add</InputLabel>
+        <Select>Add</Select>
+      </ControlContainer>
       <ItemContainer />
       <Footer />
     </>
@@ -12,23 +18,55 @@ function App() {
 }
 
 function Header() {
-  return <h1>jbjbbjb</h1>;
+  return (
+    <header>
+      <h1>ToDo App</h1>
+    </header>
+  );
 }
 
 function Footer() {
-  return <h1>jbjbbjb</h1>;
+  return (
+    <footer>
+      <p>
+        ©2023 by{" "}
+        <a href="https://janhajek.com/" className="font-bold">
+          Jan Hajek
+        </a>
+      </p>
+    </footer>
+  );
 }
 
-function ControlContainer() {
-  return <h1>jbjbbjb</h1>;
+function ControlContainer({ children }) {
+  return <>{children}</>;
 }
 
-function ItemContainer() {
-  return <h1>jbjbbjb</h1>;
+function ItemContainer({ children }) {
+  return <h3>{children}</h3>;
 }
 
-function Button() {
-  return <h1>jbjbbjb</h1>;
+function Button({ children }) {
+  return <button>{children}</button>;
+}
+
+function InputLabel() {
+  return (
+    <>
+      <label for="search">Search</label>
+      <input type="text" id="search" />
+    </>
+  );
+}
+
+function Select() {
+  return (
+    <select name="pets" id="pet-select">
+      <option value="all">All</option>
+      <option value="incomplete">Incomplete</option>
+      <option value="completed">Completed</option>
+    </select>
+  );
 }
 
 export default App;
