e24140a4402a4e6becd2495d2fe61ac993517e3b
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2023-11-26 15:12:09 +0100

    add tailwind to project

--- src/Components/Container components/Header.jsx
+++ src/Components/Container components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
       <header>
-        <h1>ToDo App</h1>
+        <h1 className="text-xl text-yellow-500 font-semibold text-center">ToDo App</h1>
       </header>
     );
   }
--- src/Components/Reusable components/Button.jsx
+++ src/Components/Reusable components/Button.jsx
@@ -1,4 +1,5 @@
 export default function Button({ children, onClick, type = "button" }) {
-  return <button onClick={onClick} type = {type}>{children}</button>;
+  return <button onClick={onClick} type = {type} className="bg-yellow-400 hover:bg-yellow-600 px-2 py-1"
+  >{children}</button>;
 }
 
